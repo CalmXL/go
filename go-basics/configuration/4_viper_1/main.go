38e7709af5573b1c6938c03f3a02f38c2dfec625
xl <xl18845898715@gmail.com>
2024-08-13 08:57:01 +0800

    feat: viper-1

--- go-basics/configuration/4_viper_1/main.go
+++ go-basics/configuration/4_viper_1/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 	"log"
 )
@@ -24,11 +26,87 @@ func main() {
 	/**
 	  2. 使用 SetConfigFile 和 ReadInConfig 读取配置文件的内容
 	*/
-	viper.SetConfigFile("./config/config.yaml")
+	// viper.SetConfigFile("./config/config.yaml")
+	// viper.SetConfigFile("./config/config1.json")
+	//
+	// if err := viper.ReadInConfig(); err != nil {
+	// 	log.Fatalf("读取配置文件失败: %s", err)
+	// 	return
+	// }
+	//
+	// appEnv := viper.Get("app_env")
+	// fmt.Println(appEnv)
+
+	/**
+	  通过 AddConfigPath SetConfigName SetConfigType ReadInConfig 读取配置文件内容
+
+		先以文件名优先级进行匹配
+	*/
+	// viper.AddConfigPath("./config")
+	// viper.SetConfigName("config")
+	// viper.SetConfigType("yaml")
+	//
+	// if err := viper.ReadInConfig(); err != nil {
+	// 	log.Fatalf("Error reading config file, %s", err)
+	// 	return
+	// }
+	// appEnv := viper.Get("app_env")
+	// fmt.Println(appEnv)
+
+	/**
+	4. 别名
+	*/
+	// viper.Set("app_env", "production")
+	// viper.RegisterAlias("env", "app_env")
+	// env := viper.Get("env")
+	//
+	// fmt.Println(env)
+
+	/**
+	  5. 设置默认值
+	*/
+
+	// viper.SetDefault("app_env", "development")
+	// viper.SetDefault("db", map[string]any{
+	// 	"host":     "127.0.0.1",
+	// 	"port":     3306,
+	// 	"user":     "root",
+	// 	"password": "xL1210...",
+	// 	"dbname":   "study",
+	// })
+	//
+	// fmt.Println(viper.Get("app_env"))
+	// fmt.Println(viper.Get("db"))
+
+	/**
+	  6.写入文件 和 监听配置变化
+	*/
+	viper.SetConfigFile("./config/db.yaml")
 	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("读取配置文件失败: %s", err)
+		log.Fatalf("Error reading config file, %s", err)
 		return
 	}
 
-	appEnv := viper.Get("app_env")
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println(e.Name)
+	})
+
+	viper.Set("db", map[string]interface{}{
+		"host":     "127.0.0.1",
+		"port":     3306,
+		"user":     "root",
+		"password": "xL1210...",
+		"dbname":   "study",
+	})
+
+	viper.Set("db.type", "mysql")
+	if err := viper.WriteConfig(); err != nil {
+		log.Fatalf("Error writing config, %s", err)
+		return
+	}
+
+	if err := viper.SafeWriteConfigAs("./config/database.yaml"); err != nil {
+		log.Fatalf("Error writing config, %s", err)
+	}
 }
